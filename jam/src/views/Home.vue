5f117bbaf376423f14a0a4957947d0ecd8f1fe1d
baptistefrtr <baptistefrtr@gmail.com>
2021-02-12 23:29:09 +0100

    Back-end link

--- jam/src/views/Home.vue
+++ jam/src/views/Home.vue
@@ -1,6 +1,6 @@
 ²<template>
     <b-row>
-      <b-col cols="4" class="back" style="padding-top: 20%">
+      <b-col cols="4" class="back" style="padding-top: 10%">
         <b-row>
           <b-col cols="3"></b-col>
           <b-col cols="6">
@@ -15,11 +15,15 @@
       </b-col>
       <b-col cols="8">
         <Shop/>
+        <button type="button" class="btn btn-primary" @click="editUser()">
+        Edit user
+      </button>
       </b-col>
     </b-row>
 </template>
 
 <script>
+import firebase from "firebase";
 import Clock from "../components/Clock.vue";
 import Shop from "../components/Shop/Shop.vue";
 
@@ -32,12 +36,59 @@ export default {
     return {
       fromChild: "",
       counter: 0,
+      pastMaterial: {
+        flint: 0,
+        fire: 0,
+        bow: 0,
+        tissage: 0,
+        town: 0,
+        wheel: 0,
+        writing: 0,
+      },
     };
   },
   methods: {
     onChildClick(value) {
       this.fromChild = value;
     },
+    editUser() {
+      const db = this.$firebase.firestore();
+      db.collection("users")
+        .doc(this.user.uid)
+        .set({
+          userVerified: this.userVerified,
+          createdID: this.user.uid,
+          clicks: this.counter,
+          time: this.counter,
+          past: this.pastMaterial,
+        })
+        .then(() => {
+          location.reload();
+        });
+    },
+    reloadPage() {
+      location.reload();
+    },
+  },
+  created() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.user = user;
+        const db = this.$firebase.firestore();
+        console.log(user.uid);
+        var storeLib = [];
+        db.collection("users")
+          .doc(user.uid)
+          .get()
+          .then((doc) => {
+            storeLib.push(doc.data());
+            this.userVerified = user.emailVerified;
+            this.testDocument = storeLib;
+          });
+      } else {
+        this.user = null;
+      }
+    });
   },
 };
 </script>
--- jam/src/views/profile/Profile.vue
+++ jam/src/views/profile/Profile.vue
@@ -1,34 +1,10 @@
 <template>
   <div class="vue-tempalte">
-    <div class="row" v-if="userVerified != true">
-      <div class="col-4"></div>
-      <div class="col-4">
-        <br /><br /><br />
-        <h3>You are not verified yet !</h3>
-        <p>Check your email and come back when you will be registered</p>
-        <br /><br />
-      </div>
-      <div class="col-4"></div>
-    </div>
-    <div class="row" v-if="userVerified != true">
-        <div class="col-4"></div>
-        <div class="col-4">
-            <button
-          type="submit"
-          class="btn btn-success btn-lg btn-block"
-          @click="editUser()"
-        >
-          Want to refresh ?
-        </button>
-        </div>
-        <div class="col-4"></div>
-    </div>
-    <div v-if="userVerified != false">
+    <div>
       <h3>Welcome</h3>
       <p>{{ user.displayName }}</p>
       <p>{{ user.email }}</p>
       <p>{{ user.id }}</p>
-      <p>{{ userVerified }}</p>
       <div class="row">
         <div class="col-4"></div>
         <div class="col-4">
@@ -42,30 +18,11 @@
           </button>
         </div>
         <div class="col-4"></div>
-        s
       </div>
-      <br /><br /><br />
-      <h3>
-        Here you are able to enable the three different services
-      </h3>
       <button type="button" class="btn btn-primary" @click="editUser()">
         Edit user
       </button>
       <br /><br />
-      <div class="row">
-        <div class="col-4" align="center">
-          <div class="card" style="width: 18rem">
-            <div class="card-body">
-              <input
-                type="checkbox"
-                aria-label="Checkbox for following text input"
-                v-model="weather"
-              />
-              <h5 class="card-title">Weather Service</h5>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -77,19 +34,7 @@ export default {
   data() {
     return {
       user: null,
-      userVerified: false,
       testDocument: [],
-      insta: false,
-      instaService: [false, false, false, false, false, false, false],
-      twitter: false,
-      twitterService: [false, false, false, false, false, false, false, false],
-      twitch: false,
-      twitchService: [false, false, false, false, false, false, false, false],
-      timer: false,
-      timerService: [false, false, false, false, false, false, false, false],
-      weather: false,
-      weatherLoc: null,
-      weatherService: [false, false, false, false, false, false, false],
     };
   },
   created() {
@@ -135,15 +80,6 @@ export default {
           createdID: this.user.uid,
           weather: this.weather,
           weatherLoc: this.weatherLoc,
-          weatherService: [
-            this.weatherService[0],
-            this.weatherService[2],
-            this.weatherService[2],
-            this.weatherService[3],
-            this.weatherService[4],
-            this.weatherService[5],
-            this.weatherService[6],
-          ],
         })
         .then(() => {
           location.reload();

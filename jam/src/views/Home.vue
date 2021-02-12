e400dd0c40958d929d9b7edfd0c60775d6abfb51
baptistefrtr <baptistefrtr@gmail.com>
2021-02-12 18:05:11 +0100

    Zonages
    Le zonage pour planter du ter ter

--- jam/src/views/Home.vue
+++ jam/src/views/Home.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="home">
+    <div class="row">
+      <div class="col-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea obcaecati nihil, nulla doloremque hic laborum laudantium doloribus ratione quo praesentium! Dicta mollitia deleniti vel voluptates maiores eveniet distinctio sequi modi.
+      </div>
+      <div class="col-8">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam doloribus dolorum laudantium consequuntur, nobis nostrum aliquam distinctio et laboriosam exercitationem totam commodi suscipit magnam itaque cum esse quia consectetur atque?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum accusantium, magni, commodi, ex deserunt vel assumenda inventore dicta dolore fuga quos nisi beatae fugiat neque iusto perspiciatis ipsum excepturi sed?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo laudantium omnis numquam nemo cupiditate nesciunt molestiae, nobis impedit recusandae, pariatur possimus repellat assumenda itaque. Provident incidunt dolores odio expedita ut.
+      </div>
+    </div>
   </div>
 </template>
 

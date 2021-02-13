4755c3fa305166cbf2cb2e7908a52c978cc79bce
baptistefrtr <baptistefrtr@gmail.com>
2021-02-13 19:39:38 +0100

    Alertbox when game saved

--- jam/src/components/Clock.vue
+++ jam/src/components/Clock.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <a v-on:click="$emit('increment')" ><img class="thumbnail" src="../assets/clock.png" width="40%" /></a>
+      <a v-on:click="$emit('increment')" ><img class="lol"  v-bind:class="[isActive ? 'style_prevu_kit' : 'blue']" @click="isActive = !isActive" src="../assets/clock.png" width="40%" /></a>
         <br><br><br>
   </div>
 </template>
@@ -9,6 +9,7 @@
 export default {
     data() {
         return {
+            isActive: true,
             counter: 0,
             show: true,
         }
@@ -19,13 +20,66 @@ export default {
     emitToParent (event) {
       this.$emit('childToParent', this.counter)
       event.childMessage;
-    }
+    },
   }
 }
 </script>
 
 <style scoped>
-.thumbnail:hover {
-  width: 41%;
+
+.blue{
+  display:inline-block;
+    border:0;
+    width:40%;
+    position: relative;
+    -webkit-transition: all 100ms ease-in;
+    -webkit-transform: scale(1); 
+    -ms-transition: all 100ms ease-in;
+    -ms-transform: scale(1); 
+    -moz-transition: all 100ms ease-in;
+    -moz-transform: scale(1);
+    transition: all 100ms ease-in;
+    transform: scale(1.05); 
+}
+
+.blue:hover {
+  z-index: 2;
+    -webkit-transition: all 100ms ease-in;
+    -webkit-transform: scale(1.5);
+    -ms-transition: all 100ms ease-in;
+    -ms-transform: scale(1.5);   
+    -moz-transition: all 100ms ease-in;
+    -moz-transform: scale(1.5);
+    transition: all 100ms ease-in;
+    transform: scale(1.05);
+}
+
+.style_prevu_kit
+{
+    display:inline-block;
+    border:0;
+    width:40%;
+    position: relative;
+    -webkit-transition: all 100ms ease-in;
+    -webkit-transform: scale(1); 
+    -ms-transition: all 100ms ease-in;
+    -ms-transform: scale(1); 
+    -moz-transition: all 100ms ease-in;
+    -moz-transform: scale(1);
+    transition: all 100ms ease-in;
+    transform: scale(1);   
+
+}
+.style_prevu_kit:hover
+{
+    z-index: 2;
+    -webkit-transition: all 100ms ease-in;
+    -webkit-transform: scale(1.5);
+    -ms-transition: all 100ms ease-in;
+    -ms-transform: scale(1.5);   
+    -moz-transition: all 100ms ease-in;
+    -moz-transform: scale(1.5);
+    transition: all 100ms ease-in;
+    transform: scale(1.05);
 }
 </style>

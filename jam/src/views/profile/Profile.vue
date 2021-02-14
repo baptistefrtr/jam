89199cf00048eb99f31be77d8c776d730f9ae7f4
baptistefrtr <baptistefrtr@gmail.com>
2021-02-15 00:41:29 +0100

    Profile & all upgrade icons

--- jam/src/views/profile/Profile.vue
+++ jam/src/views/profile/Profile.vue
@@ -1,30 +1,116 @@
 <template>
-  <div class="vue-tempalte">
-    <div>
-      <h3>Welcome</h3>
-      <p>{{ user.displayName }}</p>
-      <p>{{ user.email }}</p>
-      <p>{{ user.id }}</p>
-      <div class="row">
-        <div class="col-4"></div>
-        <div class="col-4">
-          <br /><br />
-          <button
-            type="submit"
-            class="btn btn-danger btn-lg btn-block"
-            @click="logOut()"
-          >
-            Log out
-          </button>
-        </div>
-        <div class="col-4"></div>
-      </div>
-      <button type="button" class="btn btn-primary" @click="editUser()">
-        Edit user
-      </button>
-      <br /><br />
-    </div>
-  </div>
+  <b-row class="styleMec">
+    <b-col cols="2"></b-col>
+    <b-col cols="8">
+      <h2>A track of all your stats</h2>
+      <b-row style="text-align: left">
+        <b-col>
+          <h2>Global</h2>
+        </b-col>
+      </b-row>
+      <b-row style="text-align: left">
+        <br><br><br><br>
+        <b-col cols="6">
+          <p>Username: {{ user.displayName }}</p>
+          <p>Mail: {{ user.email }}</p>
+        </b-col>
+        <b-col cols="6">
+          <p>Id: {{ testDocument[0].createdID }}</p>
+          <p>Number of click: {{ testDocument[0].clicks }}</p>
+        </b-col>
+      </b-row>
+      <b-row style="text-align: left">
+        <b-col>
+          <h2 style="text-align: left">Past</h2>
+        </b-col>
+      </b-row>
+      <b-row style="text-align: left">
+        <br><br><br><br>
+        <b-col cols="6">
+          <p>Number of flint: {{ testDocument[0].past.flint }}</p>
+          <p>Number of church: {{ testDocument[0].past.church }}</p>
+          <p>Number of town: {{ testDocument[0].past.town }}</p>
+          <p>Number of electricity: {{ testDocument[0].past.electricity }}</p>
+          <p>Number of photography: {{ testDocument[0].past.photography }}</p>
+        </b-col>
+        <b-col cols="6">
+          <p>Number of bow: {{ testDocument[0].past.bow }}</p>
+          <p>Number of writing: {{ testDocument[0].past.writing }}</p>
+          <p>Number of wheel: {{ testDocument[0].past.wheel }}</p>
+          <p>Number of boat: {{ testDocument[0].past.boat }}</p>
+          <p>Number of train: {{ testDocument[0].past.train }}</p>
+        </b-col>
+      </b-row>
+      <b-row style="text-align: left">
+        <b-col>
+          <h2 style="text-align: left">Present</h2>
+        </b-col>
+      </b-row>
+      <b-row style="text-align: left">
+        <br><br><br><br>
+        <b-col cols="6">
+          <p>Number of flint: {{ testDocument[0].present.rocket }}</p>
+          <p>Number of flint: {{ testDocument[0].present.covid }}</p>
+          <p>Number of flint: {{ testDocument[0].present.disk }}</p>
+          <p>Number of flint: {{ testDocument[0].present.computer }}</p>
+        </b-col>
+        <b-col cols="6">
+          <p>Number of flint: {{ testDocument[0].present.phone }}</p>
+          <p>Number of flint: {{ testDocument[0].present.games }}</p>
+          <p>Number of flint: {{ testDocument[0].present.coca }}</p>
+        </b-col>
+      </b-row>
+      <b-row style="text-align: left">
+        <b-col>
+          <h2 style="text-align: left">Futur</h2>
+        </b-col>
+      </b-row>
+      <b-row style="text-align: left">
+        <br><br><br><br>
+        <b-col cols="6">
+          <p>Number of flint: {{ testDocument[0].futur.motherboard }}</p>
+          <p>Number of flint: {{ testDocument[0].futur.hologram }}</p>
+        </b-col>
+        <b-col cols="6">
+          <p>Number of flint: {{ testDocument[0].futur.dolorean }}</p>
+        </b-col>
+      </b-row>
+      <b-row style="text-align: left">
+        <b-col>
+          <h2 style="text-align: left">Upgrade</h2>
+        </b-col>
+      </b-row>
+      <b-row style="text-align: left">
+        <br><br><br><br>
+        <b-col cols="6">
+          <p>Upgrade level of wheel: {{ testDocument[0].upgrade.wheel }}</p>
+          <p>Upgrade level of phone: {{ testDocument[0].upgrade.phone }}</p>
+          <p>Upgrade level of games: {{ testDocument[0].upgrade.games }}</p>
+          <p>Upgrade level of hologram: {{ testDocument[0].upgrade.hologram }}</p>
+          <p>Upgrade level of writing: {{ testDocument[0].upgrade.writing }}</p>
+          <p>Upgrade level of electricity: {{ testDocument[0].upgrade.electricity }}</p>
+          <p>Upgrade level of photography: {{ testDocument[0].upgrade.photography }}</p>
+          <p>Upgrade level of computer: {{ testDocument[0].upgrade.computer }}</p>
+          <p>Upgrade level of bow: {{ testDocument[0].upgrade.bow }}</p>
+          <p>Upgrade level of church: {{ testDocument[0].upgrade.church }}</p>
+          <p>Upgrade level of motherboard: {{ testDocument[0].upgrade.motherboard }}</p>
+        </b-col>
+        <b-col cols="6">
+          <p>Upgrade level of disk: {{ testDocument[0].upgrade.disk }}</p>
+          <p>Upgrade level of flint: {{ testDocument[0].upgrade.flint }}</p>
+          <p>Upgrade level of town: {{ testDocument[0].upgrade.town }}</p>
+          <p>Upgrade level of fire: {{ testDocument[0].upgrade.fire }}</p>
+          <p>Upgrade level of covid: {{ testDocument[0].upgrade.covid }}</p>
+          <p>Upgrade level of boat: {{ testDocument[0].upgrade.boat }}</p>
+          <p>Upgrade level of train: {{ testDocument[0].upgrade.train }}</p>
+          <p>Upgrade level of dolorean: {{ testDocument[0].upgrade.dolorean }}</p>
+          <p>Upgrade level of rocket: {{ testDocument[0].upgrade.rocket }}</p>
+          <p>Upgrade level of coca: {{ testDocument[0].upgrade.coca }}</p>
+        </b-col>
+      </b-row>
+    </b-col>
+    <b-col cols="2"></b-col>
+  </b-row>
 </template>
 
 <script>
@@ -49,6 +135,7 @@ export default {
           .get()
           .then((doc) => {
             storeLib.push(doc.data());
+            this.testDocument = storeLib;
             this.userVerified = user.emailVerified;
             this.testDocument = storeLib;
             this.weather = storeLib[0].weather;
@@ -70,8 +157,8 @@ export default {
         });
     },
     editUser() {
-        console.log("test du verified");
-        console.log(this.userVerified);
+      console.log("test du verified");
+      console.log(this.userVerified);
       const db = this.$firebase.firestore();
       db.collection("users")
         .doc(this.user.uid)
@@ -91,3 +178,11 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.styleMec {
+  background-image: url("../../assets/Shop.jpg");
+  color: white;
+  font-size: 130%;
+}
+</style>
